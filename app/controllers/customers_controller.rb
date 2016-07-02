7a46389ef1d52d40d24a89d35b0c302b25829ea0
Kikuchi Shigeo <K@sigepon.local>
2016-07-02 09:11:27 +0900

    CRUD Editまで実装

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,20 +3,61 @@ class CustomersController < ApplicationController
   end
 
   def new
+    @custmoer = Custmer.new
   end
 
   def create
+    @custmoer = Customer.new(custmoer_params)
+    if @custmoer.save
+      redirect_to @custmoer
+    else
+      render :new
+    end
   end
 
   def edit
+    @custmoer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render :edit
+    end
   end
 
   def show
+    @custmoer = Customer.faind(params[:id])
   end
 
   def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect_to customer_url
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :family_name,
+      :given_name,
+      :email
+    )
+
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,12 @@
 class Customer < ActiveRecord::Base
+    full_name = family_name + given_name
+    full_name
+
+    validates :family_name,
+        presence: true, length: {maximum: 20}
+    validates :given_name,
+        presence: true, length: {maximum: 20}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email,
+        presence: true, format: { with: VALID_EMAIL_REGEX }
 end
